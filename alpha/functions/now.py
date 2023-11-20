d5c261aacdbfa8c297a127502d2c9e8a6d2fe275
sejong <112914777+se-jong-king@users.noreply.github.com>
2023-11-21 05:27:10 +0900

    Add files via upload

---
+++ alpha/functions/now.py
@@ -0,0 +1,13 @@
+#현재 시간 알려주는 기능
+
+import json
+from datetime import datetime
+import pytz
+
+def now(tz:str="Asia/Seoul"):
+  """Get the current date and time.
+  """
+  return json.dumps({
+    "timezone": tz,
+    "time": datetime.now(pytz.timezone(tz)).strftime("%Y-%m-%d %H:%M:%S"),
+  })
