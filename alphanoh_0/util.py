438cd3469369fb19f663186e5b0470ed3dc288a1
sejong <112914777+se-jong-king@users.noreply.github.com>
2023-11-21 14:52:30 +0900

    Update util.py

--- alphanoh_0/util.py
+++ alphanoh_0/util.py
@@ -3,7 +3,7 @@ from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.docstore.document import Document
 
 from langchain.chat_models import ChatOpenAI
-from debug import FakeChatModel
+from alphanoh_0.debug import FakeChatModel
 from langchain.chat_models.base import BaseChatModel
 
 
